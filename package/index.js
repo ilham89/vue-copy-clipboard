760e98c8205672c82aa68d72b13e77edb36c86e5
ilham89 <ilham.kodingworks@gmail.com>
2022-08-13 14:30:59 +0700

    add readme

---
+++ package/index.js
@@ -0,0 +1,18 @@
+const copyToClipboard = (text) => {
+    let input = document.createElement("input");
+    input.setAttribute("value", text);
+    document.body.appendChild(input);
+    input.select();
+    let result = document.execCommand("copy");
+    document.body.removeChild(input);
+    return result;
+};
+
+export const useCopyClipboard = () => {
+    return (text) => {
+        if (typeof text === "string" || typeof text == "number") {
+            return copyToClipboard(text);
+        }
+        return false;
+    };
+};
